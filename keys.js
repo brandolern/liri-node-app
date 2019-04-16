327e551bf8f3e754e74dc398237a8be152e32c47
brandolern <46857695+brandolern@users.noreply.github.com>
2019-04-15 19:12:19 -0600

    Added spotify

--- keys.js
+++ keys.js
@@ -4,4 +4,3 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
-// Omdb api key: b4d60128
--- liri.js
+++ liri.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var fs = require("fs");
 var moment = require("moment");
 
-//Variables for the 4 different functionalities
+var spotify = new Spotify(keys.spotify);
+
+
+//Variables for the programs 4 different commands
 var concert = "concert-this";
 var searchSpotify = "spotify-this-song";
 var movie = "movie-this";
@@ -50,11 +53,38 @@ switch (input) {
 
         //Spotify Api
     case searchSpotify:
+        search();
+
+        spotify.search({
+            type: 'track',
+            query: userSearch
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            for (i = 0; i < data.tracks.items.length; i++) {
+
+                console.log("```")
+                console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
+                console.log("Song Name: " + data.tracks.items[i].name);
+
+                if (data.tracks.items[i].preview_url !== null) {
+                    console.log("Preview: " + data.tracks.items[i].preview_url);
+                } else {
+                    console.log("Preview: unavailable")
+                }
+
+                console.log("Album: " + data.tracks.items[i].album.name);
+            }
+        });
+
         break
 
         //OMDB Api
     case movie:
 
+        //Runs if the user typed in a movie name
         if (process.argv.length < 4) {
 
             axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=b4d60128").then(function (response) {
@@ -70,20 +100,32 @@ switch (input) {
                 console.log("```");
 
             }).catch(function (err) {
-                console.log(err);
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
             });
             return
         }
 
+        //Default if the user didn't search for a movie
         search();
         var omdbQuery = `http://www.omdbapi.com/?t=${userSearch}&y=&plot=short&apikey=b4d60128`;
         axios.get(omdbQuery).then(function (response) {
-            console.log(response.data)
+
             console.log("```");
             console.log("Title: " + response.data.Title);
             console.log("Release Year: " + response.data.Released);
             console.log("IMDB Rating: " + response.data.imdbRating);
 
+            //If statement that removes rotten tomatoes rating if their isn't one (ex: tv shows).
             if (response.data.Ratings.length > 1) {
                 console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
             }
@@ -116,23 +158,3 @@ switch (input) {
     default:
         break
 }
-
-
-
-
-
-// var spotify = new Spotify(keys.spotify);
-
-
-
-// spotify
-//     .search({
-//         type: 'track',
-//         query: 'All the Small Things'
-//     })
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (err) {
-//         console.log(err);
-//     });
