1c186300bff7124d5e056889e4afd6f87ae87c57
brandolern <46857695+brandolern@users.noreply.github.com>
2019-04-15 23:23:08 -0600

    restructed code into an object

--- liri.js
+++ liri.js
@@ -7,36 +7,34 @@ var moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
 
+var liri = {
+    commands: {
+        concert: 'concert-this',
+        spotify: 'spotify-this-song',
+        movie: 'movie-this',
+        doWhatItSays: 'do-what-it-says'
+    },
 
-//Variables for the programs 4 different commands
-var concert = "concert-this";
-var searchSpotify = "spotify-this-song";
-var movie = "movie-this";
-var doWhatItSays = "do-what-it-says";
+    nodeArgs: process.argv,
 
-var nodeArgs = process.argv;
-var userSearch = "";
+    input: process.argv[2],
 
-function search() {
-    for (var i = 3; i < nodeArgs.length; i++) {
+    userSearch: "",
 
-        if (i > 3 && i < nodeArgs.length) {
-            userSearch = userSearch + "+" + nodeArgs[i];
+    search: function () {
+        for (var i = 3; i < this.nodeArgs.length; i++) {
 
-        } else {
-            userSearch += nodeArgs[i];
+            if (i > 3 && i < this.nodeArgs.length) {
+                this.userSearch = this.userSearch + "+" + this.nodeArgs[i];
+
+            } else {
+                this.userSearch += this.nodeArgs[i];
+            }
         }
-    }
-}
+    },
 
-var input = process.argv[2];
-
-switch (input) {
-
-    //Bands in town api
-    case concert:
-        search();
-        var concertQuery = `https://rest.bandsintown.com/artists/${userSearch}/events?app_id=codingbootcamp`;
+    concertSearch: function () {
+        var concertQuery = `https://rest.bandsintown.com/artists/${this.userSearch}/events?app_id=codingbootcamp`;
         axios.get(concertQuery).then(
             function (response) {
                 for (i = 0; i < response.data.length; i++) {
@@ -47,77 +45,47 @@ switch (input) {
                 }
             }).catch(function (err) {
             console.log(err);
-        });;
+        });
+    },
 
-        break
+    spotifySearch: function () {
 
-        //Spotify Api
-    case searchSpotify:
-        search();
+        if (this.nodeArgs.length < 4) this.userSearch = "The+Sign";
 
         spotify.search({
-            type: 'track',
-            query: userSearch
-        }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-
-            for (i = 0; i < data.tracks.items.length; i++) {
-
-                console.log("```")
-                console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
-                console.log("Song Name: " + data.tracks.items[i].name);
-
-                if (data.tracks.items[i].preview_url !== null) {
-                    console.log("Preview: " + data.tracks.items[i].preview_url);
-                } else {
-                    console.log("Preview: unavailable")
+                type: 'track',
+                query: this.userSearch
+            },
+            function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
                 }
 
-                console.log("Album: " + data.tracks.items[i].album.name);
-            }
-        });
+                for (i = 0; i < data.tracks.items.length; i++) {
 
-        break
+                    console.log("```")
+                    console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
+                    console.log("Song Name: " + data.tracks.items[i].name);
 
-        //OMDB Api
-    case movie:
+                    if (data.tracks.items[i].preview_url !== null) {
+                        console.log("Preview: " + data.tracks.items[i].preview_url);
+                    } else {
+                        console.log("Preview: unavailable")
+                    };
 
-        //Runs if the user typed in a movie name
-        if (process.argv.length < 4) {
-
-            axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=b4d60128").then(function (response) {
-                console.log("```");
-                console.log("Title: " + response.data.Title);
-                console.log("Release Year: " + response.data.Released);
-                console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-                console.log("Country: " + response.data.Country);
-                console.log("Language: " + response.data.Language);
-                console.log("Plot: " + response.data.Plot);
-                console.log("Actors: " + response.data.Actors);
-                console.log("```");
-
-            }).catch(function (err) {
-                if (error.response) {
-                    console.log(error.response.data);
-                    console.log(error.response.status);
-                    console.log(error.response.headers);
-                } else if (error.request) {
-
-                    console.log(error.request);
-                } else {
-                    console.log("Error", error.message);
-                }
-                console.log(error.config);
+                    console.log("Album: " + data.tracks.items[i].album.name);
+                };
             });
-            return
+    },
+
+    movieSearch: function () {
+
+        // Checks to see if user entered a movie, searches for a default movie if they didn't
+        if (this.nodeArgs.length < 4) {
+            this.userSearch = "mr+nobody";
         }
 
-        //Default if the user didn't search for a movie
-        search();
-        var omdbQuery = `http://www.omdbapi.com/?t=${userSearch}&y=&plot=short&apikey=b4d60128`;
+        var omdbQuery = `http://www.omdbapi.com/?t=${this.userSearch}&y=&plot=short&apikey=b4d60128`;
         axios.get(omdbQuery).then(function (response) {
 
             console.log("```");
@@ -125,7 +93,7 @@ switch (input) {
             console.log("Release Year: " + response.data.Released);
             console.log("IMDB Rating: " + response.data.imdbRating);
 
-            //If statement that removes rotten tomatoes rating if their isn't one (ex: tv shows).
+            //If statement that removes rotten tomatoes rating if there isn't one (ex: tv shows).
             if (response.data.Ratings.length > 1) {
                 console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
             }
@@ -148,13 +116,74 @@ switch (input) {
                 console.log("Error", error.message);
             }
             console.log(error.config);
-        })
+        });
 
+    },
+
+    grabText: function () {
+
+        fs.readFile("random.txt", "utf8", function (err, data) {
+
+            if (err) {
+                return console.log(err);
+            };
+
+            var dataArr = data.split(",");
+            var newCommand = dataArr[0];
+            var newSearch = dataArr[1].split(" ");
+            liri.nodeArgs.splice(2, 1, newCommand);
+
+            for (i = 0; i < newSearch.length; i++) {
+                liri.nodeArgs.push(newSearch[i]);
+            };
+
+            liri.search();
+
+            if (liri.nodeArgs[2] === liri.commands.movie) liri.movieSearch();
+            else if (liri.nodeArgs[2] === liri.commands.spotify) liri.spotifySearch();
+            else if (liri.nodeArgs[2] === liri.commands.concert) liri.concertSearch();
+            else {
+                console.log("```")
+                console.log("Please enter one of the following commands on random.txt:");
+                console.log("concert-this");
+                console.log("spotify-this-song");
+                console.log("movie-this");
+                console.log("do-what-it-says");
+
+            }
+        });
+    }
+};
+
+switch (liri.input) {
+
+    //Bands in town api
+    case liri.commands.concert:
+        liri.search();
+        liri.concertSearch();
         break
 
-    case doWhatItSays:
+        //Spotify Api
+    case liri.commands.spotify:
+        liri.search();
+        liri.spotifySearch();
+        break
+
+        //OMDB Api
+    case liri.commands.movie:
+        liri.search();
+        liri.movieSearch();
+        break
+
+        //Pulls the info from random.txt
+    case liri.commands.doWhatItSays:
+        liri.grabText();
         break
 
     default:
-        break
-}
+        console.log("Please enter one of the following commands:");
+        console.log("concert-this");
+        console.log("spotify-this-song");
+        console.log("movie-this");
+        console.log("do-what-it-says");
+};
